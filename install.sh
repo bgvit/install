476c1244431f4c30f5e1323b70d0f8c341c9062b
Bernardo Gabriel Vitor <bernardo.vitor@movile.com>
2023-01-02 03:32:45 -0300

    Updated programs and finally I could configure Neovim using AstroNvim

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Creating this config file because it'll be used.
+mkdir $HOME/.config
+
 #Install nix
 curl -L https://nixos.org/nix/install | sh
 
@@ -11,10 +14,16 @@ nix-env -iA \
     nixpkgs.antibody \
     nixpkgs.asdf-vm \
     nixpkgs.bat \
+    nixpkgs.bottom \
     nixpkgs.direnv \
+    nixpkgs.elixir \
+    nixpkgs.python \
     nixpkgs.fzf \
+    nixpkgs.gdu \
     nixpkgs.git \
     nixpkgs.git-extras \
+    nixpkgs.go \
+    nixpkgs.lazygit \
     nixpkgs.neovim \
     nixpkgs.ripgrep \
     nixpkgs.stow \
@@ -34,3 +43,10 @@ cp .zsh_plugins.txt ~
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
 # Develop installation of plugins.
+
+# Configure AstroNvim
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
